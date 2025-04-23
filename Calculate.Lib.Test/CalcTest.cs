37037ccfc121f6a46e9fc36e97388f143def28bb
Starinin Andrey <starinin-andrey@ya.ru>
2025-04-23 09:57:43 +0300

    Init

---
+++ Calculate.Lib.Test/CalcTest.cs
@@ -0,0 +1,22 @@
+﻿namespace Calculate.Lib.Test;
+
+public class CalcTest
+{
+    [Fact]
+    public void AddTest()
+    {
+        const double expected = 4;
+        var actual = Calc.Add(2, 2);
+        
+        Assert.Equal(expected, actual);
+    }
+    
+    [Fact]
+    public void SubTest()
+    {
+        const double expected = 0;
+        var actual = Calc.Sub(2, 2);
+        
+        Assert.Equal(expected, actual);
+    }
+}
---
+++ Calculate.Lib/Calc.cs
@@ -0,0 +1,7 @@
+﻿namespace Calculate.Lib;
+
+public static class Calc
+{
+    public static double Add(double a, double b) => a + b;
+    public static double Sub(double a, double b) => a - b;
+}
